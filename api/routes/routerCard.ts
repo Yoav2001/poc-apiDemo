caeb8a216d554be60fc56ea7a7a82603ad0b27dc
yoav2001 <yoavelkana@gmail.com>
2021-11-17 16:52:11 +0200

    add json data

--- api/routes/routerCard.ts
+++ api/routes/routerCard.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import controlles from '../controlles'
 
+
+
 const router = express.Router();
-router.get('/',controlles.AllData);//first card
+router.get('/card/firstCard',controlles.AllData);//first card
 router.get('/card/:CardId',controlles.getTheCard );//FA /QA
 //get a card id and return all final answers 
 router.get('/finalAnswerOfCard/:CardId',controlles.getallFinalAnswersOfCardQuestion);
@@ -12,7 +14,7 @@ router.get('/MostClicked/:CardId',controlles.getMostPopularFinalAnswer);
 //gets a cardID and sends back all the FA ordered by most clicked
 // router.get('/MostClicked/:CardId',controlles.getMostClickedInTheTree);
 // //get all card that ahmash is true
-// router.get('/InchargeSelected',controlles.getInchargeSelected);//
+router.get('/InchargeSelected',controlles.getInchargeSelected);//
 
 // router.get('/dataarray',controlles.getDataArray)
 // router.get('/Answers',controlles.getAnswerArray)
